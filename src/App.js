a7d908f09f7c75d3d492a009b2006f248eaec617
Jason Boerner <jboerner56@gamil.com>
2019-04-29 11:09:19 -0400

    axios setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Axios from 'axios';
+// install axios, optional
+// need a class component
+// add ajax requests (componentDidMount it the "earliest")
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      character: {}
+    };
+  }
+  async componentDidMount() {
+    const response = await Axios.get('url to API')
+    this.setState({
+      character: response.data
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          <li>name: {this.state.character.name}</li>
+          <li>born: {this.state.character.born}</li>
+          <li>culture: {this.state.character.culture}</li>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App;
